ed213f9d3eab81eb7d8f234a159f3c326fb8dfa5
banafshaahmadyar <ahmadyarbanafsha@gmail.com>
2023-08-23 20:06:01 +0000

    Created all functionaltiy of this app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from "./components/Navigation";
+import Home from "./components/Home";
+import Students from "./components/Students";
+import Manage from "./components/Manage";
+import { BrowserRouter as Router, Switch, Route,  } from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div className="App">
-        <h2>Hello! React</h2>
-    </div>
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+       <Switch>
+         <Route exact path="/"> <Home/> </Route>
+         <Route path="/students"><Students/></Route>
+         <Route path="/manage"><Manage/></Route>
+         </Switch>
+       </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
